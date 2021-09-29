fc5d2084ca6e2a12cc2441015d5a78c4b0feec4d
tbotts <tbotts@my365.bellevue.edu>
2021-09-28 23:20:58 -0400

    Create Module 12

---
+++ module_12/README.md
@@ -0,0 +1,127 @@
+# Module 12 — WhatABook Final Program
+
+## MAIN MENU Output
+```
+Welcome to WhatABook! 
+
+MAIN MENU
+     1. View Books
+     2. View Store Locations
+     3. My Account
+     4. Exit Program
+
+Please Enter Selection: 
+  ```
+
+## View Books Option Output
+```
+LIST OF BOOKS
+
+     Book ID Number: 1
+     Book Name: Anna Karenina
+     Author: Leo Tolstoy
+     Details: Published 1878
+
+
+     Book ID Number: 2
+     Book Name: Madame Bovary
+     Author: Gustave Flaubert
+     Details: Published 1856
+
+
+     Book ID Number: 3
+     Book Name: War and Peace
+     Author: Leo Tolstoy
+     Details: Published 1869
+
+
+     Book ID Number: 4
+     Book Name: The Great Gatsby
+     Author: F. Scott Fitzgerald
+     Details: Published 1925
+
+
+     Book ID Number: 5
+     Book Name: Lolita
+     Author: Vladimir Nabokov
+     Details: Published 1955
+
+
+     Book ID Number: 6
+     Book Name: Middlemarch
+     Author: George Eliot
+     Details: Published 1871
+
+
+     Book ID Number: 7
+     Book Name: The Adventures of Huckleberry Finn
+     Author: Mark Twain
+     Details: Published 1884
+
+
+     Book ID Number: 8
+     Book Name: In Search of Lost Time
+     Author: Marcel Proust
+     Details: Published 1913
+
+
+     Book ID Number: 9
+     Book Name: Night
+     Author: Elie Wiesel
+     Details: Published 1960
+
+
+MAIN MENU
+     1. View Books
+     2. View Store Locations
+     3. My Account
+     4. Exit Program
+
+Please Enter Selection: 
+```
+
+## View Store Locations Option Output
+```
+STORE LOCATIONS
+
+     Locale: 123 Main Street Disney, FL 32837
+     Hours: 9:00AM to 9:00PM
+
+
+MAIN MENU
+     1. View Books
+     2. View Store Locations
+     3. My Account
+     4. Exit Program
+
+Please Enter Selection: 
+```
+
+## My Account Option Output
+```
+Enter User ID Number: 
+
+MY ACCOUNT MENU
+
+     1. Wishlist
+     2. Add Book
+     3. Main Menu
+
+Please Enter Selection: 
+```
+
+## Exit Program Option Output
+```
+MAIN MENU
+     1. View Books
+     2. View Store Locations
+     3. My Account
+     4. Exit Program
+
+Please Enter Selection: 4
+
+
+THANK YOU FOR USING WHATABOOK! 
+
+PROGRAM TERMINATED
+```
---
+++ module_12/whatabook.sql
@@ -0,0 +1,118 @@
+/*
+    Title: whatabook.sql
+    Author: Tara Botts
+    Date: September 28, 2021
+    Description: Module 12 WhatABook Assignment
+*/
+
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
+/*
+    Create table(s)
+*/
+CREATE TABLE store (
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
+);
+
+CREATE TABLE book (
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
+);
+
+CREATE TABLE user (
+    user_id         INT         NOT NULL    AUTO_INCREMENT,
+    first_name      VARCHAR(75) NOT NULL,
+    last_name       VARCHAR(75) NOT NULL,
+    PRIMARY KEY(user_id) 
+);
+
+CREATE TABLE wishlist (
+    wishlist_id     INT         NOT NULL    AUTO_INCREMENT,
+    user_id         INT         NOT NULL,
+    book_id         INT         NOT NULL,
+    PRIMARY KEY (wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY (book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_Id)
+);
+
+/*
+    insert store record 
+*/
+INSERT INTO store(locale)
+    VALUES('123 Main St., Disney, FL 34647');
+
+/*
+    insert book records 
+*/
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('1', 'Anna Karenina', 'Published 1878', 'Leo Tolstoy');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('2', 'Madame Bovary', 'Published 1856', 'Gustave Flaubert');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('3', 'War and Peace', 'Published 1869', 'Leo Tolstoy');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('4', 'The Great Gatsby', 'Published 1925', 'F. Scott Fitzgerald'); 
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('5', 'Lolita', 'Published 1955', 'Vladimir Nabokov');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('6', 'Middlemarch', 'Published 1871', 'George Eliot');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('7', 'The Adventures of Huckleberry Finn', 'Published 1884', 'Mark Twain');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('8', 'In Search of Lost Time', 'Published 1913', 'Marcel Proust');
+
+INSERT INTO book(book_id, book_name, details, author)
+    VALUES('9', 'Night', 'Published 1960', 'Elie Wiesel');
+
+/*
+    insert user
+*/ 
+INSERT INTO user(user_id, first_name, last_name) 
+    VALUES('1', 'Bradley', 'Davis');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('2', 'Alessa', 'Barton');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('3', 'Chelsey', 'Cooper');
+
+/*
+    insert wishlist records 
+*/
+INSERT INTO wishlist(user_id, book_id) 
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Bradley'), 
+        (SELECT book_id FROM book WHERE book_name = 'Lolita')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Alessa'),
+        (SELECT book_id FROM book WHERE book_name = 'War and Peace')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Chelsey'),
+        (SELECT book_id FROM book WHERE book_name = 'Night')
+    );
